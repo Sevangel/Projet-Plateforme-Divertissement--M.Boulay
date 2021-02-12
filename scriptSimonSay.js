04f5f2daf51c35f260f287220bc8579f5d7dfe2e
Sevangel <57137525+Sevangel@users.noreply.github.com>
2021-02-12 03:01:51 +0100

    Simon Says Game Up

---
+++ scriptSimonSay.js
@@ -0,0 +1,122 @@
+// SIMON SAYS
+
+// Variables
+
+let SimonTabCache = []
+let Green = document.querySelector('.Green')
+let Red = document.querySelector('.Red')
+let Yellow = document.querySelector('.Yellow')
+let Blue = document.querySelector('.Blue')
+let Counter = document.querySelector('#Counter')
+let ArrColors = [Red, Yellow, Blue, Green]
+let PlayerTab = []
+
+MemoryPlayerClick()
+
+// Récupérer le bouton start pour démarrer le jeu + preventDefault car comme c'est une balise a avec un lien # cela nous ramène au début de la page
+
+document.querySelector('.StartButtonSimon').addEventListener('click', function(event) {
+    event.preventDefault();
+    Game()
+})
+
+// Sleep fonction
+
+// function sleep(ms) {   
+//     return new Promise (resolve => setTimeout(resolve, ms))
+// }
+
+// Fonction pour reset le score
+
+function ClearScore() {
+    Counter.innerHTML = 0;
+    SimonTabCache = []
+    PlayerTab = []
+}
+
+// Fonction principale du jeu
+
+function Game() {
+    ClearScore()
+    ColorSequency()    
+}
+
+// Boucle pour créer la séquence de couleurs lors de cette partie
+
+
+function ColorSequency () {
+    for (let i = 0; i < 3; i++) {
+        let SimonRandomColor = ArrColors[Math.floor(Math.random() * ArrColors.length)]
+        SimonTabCache.push(SimonRandomColor)
+               
+    }
+    console.log(SimonTabCache)  
+    LightUp(SimonTabCache) 
+}
+
+// Fonction pour Eclairer la séquence
+
+function LightUp (SimonTabCache) {
+    let i = 0;
+    let Timer = setInterval(function(){
+        Lighten(SimonTabCache[i])
+    i++
+    if (i >= SimonTabCache.length) {
+      clearInterval(Timer)
+    }
+  }, 800)
+}
+
+function Lighten (SimonTabCache) {
+    SimonTabCache.classList.add('light')
+    window.setTimeout(function () {
+        SimonTabCache.classList.remove('light')
+    }, 500)
+}
+
+// Fonction pour récupérer la liste du joueur
+
+function MemoryPlayerClick () {
+    Green.addEventListener('click', () => {
+        PlayerTab.push(Green)
+        console.log(PlayerTab)
+        CheckPass()
+    })
+    Red.addEventListener('click', () => {
+        PlayerTab.push(Red)
+        console.log(PlayerTab)
+        CheckPass()
+    })
+    Blue.addEventListener('click', () => {
+        PlayerTab.push(Blue)
+        console.log(PlayerTab)
+        CheckPass()
+    })
+    Yellow.addEventListener('click', () => {
+        PlayerTab.push(Yellow)
+        console.log(PlayerTab)
+        CheckPass()
+    })
+}
+
+// Vérification de la réussite
+
+function CheckPass() {
+    for (i = 0; i < PlayerTab.length; i++) {
+        if (PlayerTab[i] != SimonTabCache[i]) {
+            alert("Quel dommage...")
+            ClearScore()
+            return
+        }
+    }
+    if (PlayerTab.length == SimonTabCache.length) {
+        if (PlayerTab.length >= 15) {
+            alert("VOUS AVEZ GAGNE UN IPHONE X")
+            ClearScore()
+        }
+        else {
+            PlayerTab = []
+            ColorSequency()
+        }   
+    }
+}
